113f23b8217915f55ed991ab5e04923bd9dee754
Marcos Gómez Vilches <marcosmgv@gmail.com>
2017-03-16 10:32:10 +0100

    Catch errors on PHP7

--- WorkerSingle.php
+++ WorkerSingle.php
@@ -216,6 +216,12 @@ class WorkerSingle
 			\Resque_Event::trigger('afterFork', $job);
 			$job->perform();
 		}
+		catch (\Error $e)
+        	{
+            		$this->logger->log(LogLevel::CRITICAL, '{job} has failed {stack}', array('job' => $job, 'stack' => $e));
+            		$job->fail($e);
+            		return;
+        	}
 		catch(\Exception $e) {
 			$this->logger->log(LogLevel::CRITICAL, '{job} has failed {stack}', array('job' => $job, 'stack' => $e));
 			$job->fail($e);
